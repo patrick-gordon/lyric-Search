bfe59203cd01720a728ca611cd540271623aad28
Patrick Gordon <patrickgordon42@yahoo.com>
2019-08-11 21:33:01 -0500

    got home page and top 10 list done, working on Lyrics.js

--- lyric-search/src/App.js
+++ lyric-search/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Navbar from "./Components/layout/Navbar";
 import Index from "./Components/layout/Index";
+import Lyrics from './Components/tracks/Lyrics'
 
 import { Provider } from "./context";
 
@@ -15,6 +16,7 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={Index} />
+              <Route exact path='/lyrics/track/:id' component={Lyrics} />
             </Switch>
           </div>
         </>
---
+++ lyric-search/src/Components/tracks/Lyrics.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Spinner from "../layout/Spinner";
+
+export default class Lyrics extends Component {
+  // only need this state for this small component so no need to use context API
+  state = {
+    track: {},
+    lyrics: {}
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/tracks.lyrics.get?track_id=${
+          this.props.match.params.id
+        }&apikey=${process.env.REACT_APP_MM_KEY}`
+      )
+      .then(res => {
+        this.setState({ lyrics: res.data.message.body.lyrics });
+
+        return axios.get(
+          `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/tracks.get?track_id=${
+            this.props.match.params.id
+          }&apikey=${process.env.REACT_APP_MM_KEY}`
+        );
+      })
+      .then(res => {
+        // console.log(res.data);
+        this.setState({ track: res.data.message.body.track });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    const { track, lyrics } = this.state;
+    if (
+      track === undefined ||
+      lyrics === undefined ||
+      Object.keys(track).length === 0 ||
+      Object.keys(lyrics).length === 0
+    ) {
+      return <Spinner />
+    } else {
+      return <h1>Data Returned</h1>;
+    }
+  }
+}
